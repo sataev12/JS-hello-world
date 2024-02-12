e29c25654ef7bbe0b04d04e39c63d1156367f4a2
John Doe11 <sataevmagomed.eu@gmail.com>
2024-02-12 23:28:05 +0100

    Differents exercices de JavaScript

--- hello-world/js/main.js
+++ hello-world/js/main.js
@@ -23,3 +23,25 @@ console.log("1 : " + (2 == "2"));
 console.log("2 : " + (2 === "2"));
 console.log("3 : " + (2 != "2"));
 console.log("4 : " + (2 !== "2"));
+
+console.log("3 < 7 is" + (3 < 7)); //TRUE
+console.log("8 <= 1 is" + (8 <= 1));//FALSE
+console.log("7 >= 7 is" + (7 >= 7));//TRUE
+console.log("7 > 7 is" + (7 > 7)); //FALSE
+
+const ageJohn = 20;
+console.log("John est " + (ageJohn >= 18 ? "majeur" : "mineur") + " en France."); //concatenation
+console.log(`John est ${ageJohn >= 21 ? "majeur" : "mineur"} aux Etats-Unis.`); //interpolation
+
+for (let i = 0; i < 10; i++) {
+    console.log(`3 x ${i} = ${3 * i}`);
+}
+
+for (let i = 0; i < 3; i++) {
+    console.log("Je serai un bon développeur");
+}
+
+for (let i = 3; i < 10; i+= 3) {
+    console.log(`${i} est inférieur ou égal à 10 et est divisible par 3`);
+    
+}
