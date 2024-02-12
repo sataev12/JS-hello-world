f4576d6fbfa0519bdfbf6e2ea8d1c1052ec0feb1
John Doe11 <sataevmagomed.eu@gmail.com>
2024-02-12 22:36:41 +0100

    first commit

---
+++ hello-world/js/main.js
@@ -0,0 +1,25 @@
+console.log("Hello JS")
+
+
+let numberToGuess = 2;
+numberToGuess +=3;
+numberToGuess -=2;
+numberToGuess++;
+numberToGuess*=10;
+numberToGuess/=2;
+numberToGuess%=7;
+numberToGuess--;
+
+console.log(numberToGuess);
+
+const prenom = "John";
+const nom = "Smith";
+const nomComplet = prenom + " " + nom;
+console.log("Je m'appelle " + nomComplet);
+
+console.log(`Je m'appelle ${prenom} ${nom}, soyez le bienvenu !`);
+
+console.log("1 : " + (2 == "2"));
+console.log("2 : " + (2 === "2"));
+console.log("3 : " + (2 != "2"));
+console.log("4 : " + (2 !== "2"));
